a6277046e01f63008b89ecffc95a4565cf3eb62a
Rage Gaming <72497418+Rage-Gaming@users.noreply.github.com>
2022-11-16 11:08:09 +0530

    Update main.js

--- Asset/main.js
+++ Asset/main.js
@@ -1,5 +1,5 @@
 // Setup End Date for Countdown (getTime == Time in Milleseconds)
-let launchDate = new Date("May 1, 2022 15:00:00").getTime();
+let launchDate = new Date("November 20, 2022 15:00:00").getTime();
 
 // Setup Timer to tick every 1 second
 let timer = setInterval(tick, 1000);
